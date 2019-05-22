cb2d04efb7b5e7519f2ffc5278f656b3e9e8e2af
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-05-22 12:27:23 +0700

    add keys into settings

--- recaptcha/recaptcha/settings.py
+++ recaptcha/recaptcha/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+RECAPTCHA_SITE_KEY = "6Le60KQUAAAAAHgO7i5xBOQCLb3rbb9y9s02EqeW"
+RECAPTCHA_SECRET_KEY = "6Le60KQUAAAAAOANCQE8eVZNvL-VdP13lyEujLsq"
--- recaptcha/recaptcha_example/views.py
+++ recaptcha/recaptcha_example/views.py
@@ -1,13 +1,15 @@
 """Simple views for handle recaptcha v3 example"""
 
 from django.shortcuts import render, redirect
+from django.conf import settings
 
 
 def index(request):
     """
     simple render index.html
     """
-    return render(request, 'index.html')
+
+    return render(request, 'index.html', {'site_key': settings.RECAPTCHA_SITE_KEY})
 
 
 def contact(request):
