50d4f25423c25ad6c64bafaa5f830b3d644bc7d2
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-05-22 11:18:56 +0700

    add view

--- recaptcha/recaptcha_example/views.py
+++ recaptcha/recaptcha_example/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+"""Simple views for handle recaptcha v3 example"""
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    """
+    simple render index.html
+    """
+    return render(request, 'index.html')
+
+
+def contact(request):
+    """simple handle contact post"""
+    if not request.method == 'POST':
+        return redirect('/')
+
+    data = request.POST
+    name = data.get('name')
+
+    return render(request, 'contact_sent.html', {'name': name})
